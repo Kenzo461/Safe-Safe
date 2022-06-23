d7a6d8342c7fe11d1953fdf227cf944893848660
Kenzo461 <107798869+Kenzo461@users.noreply.github.com>
2022-06-23 23:18:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fetch = require('node-fetch');
 const moment = require('moment');
 const chalk = require('chalk');
 const rs = require('readline-sync');
+const chalkRed = require('chalk-red')
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
